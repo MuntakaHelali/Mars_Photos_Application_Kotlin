dba77a20f66fea768f16281b3f3020f41039cc66
Muntaka Helali <helali.muntaka@gmail.com>
2022-04-30 05:08:26 -0400

    Added comments to make the code more understandable.

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -9,21 +9,28 @@ import retrofit2.http.GET
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
 
+//Building the Moshi object that will be used with Retrofit
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
+//Building the Retrofit Object with Moshi converter
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
-
+//A public interface that exposes the getPhotos method to the rest of the app
 interface MarsApiService {
+    /**
+     * Returns a [List] of [MarsPhoto] and this method can be called from a Coroutine.
+     * The @GET annotation indicates that the "photos" endpoint will be requested with the GET
+     * HTTP method
+     */
     @GET("photos")
     suspend fun getPhotos(): List<MarsPhoto>
 }
-
+//A public Api object that exposes the lazy-initialized Retrofit service
 //object declaration of singleton, ensuring that one, and only one, instance of the object is created, has on global point of access
 object MarsApi {
     // lazy instantiation is when object creation is purposely delayed until you actually need that object to avoid unnecessary computation or resources
